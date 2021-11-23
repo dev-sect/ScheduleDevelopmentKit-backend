64cd75e6be5b4fccc90aa625b27f45b5d2946ee3
Bibletoon <alex8.ivanov@gmail.com>
2021-11-23 11:22:11 +0300

    feat: add email validation

---
+++ src/ScheduleDevelopmentKit.DataAccess/EntityConfigurations/EmailConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ScheduleDevelopmentKit.Domain.ValueObjects;
+
+namespace ScheduleDevelopmentKit.DataAccess.EntityConfigurations
+{
+    public class EmailConfiguration : IEntityTypeConfiguration<Email>
+    {
+        public void Configure(EntityTypeBuilder<Email> builder)
+        {
+            builder.Property(e => e.Value).HasMaxLength(60);
+        }
+    }
+}
