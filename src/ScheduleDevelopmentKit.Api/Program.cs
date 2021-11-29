f6edc5b0f9fee47201de79aebeb4ecec3aedb31d
Bibletoon <alex8.ivanov@gmail.com>
2021-11-29 11:07:24 +0300

    Feat: added logger

--- src/ScheduleDevelopmentKit.Api/Program.cs
+++ src/ScheduleDevelopmentKit.Api/Program.cs
@@ -2,11 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using ScheduleDevelopmentKit.Api;
 using ScheduleDevelopmentKit.Api.Infrastructure.Extensions;
+using Serilog;
 
 CreateHostBuilder(args).Build().Run();
 
 static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, configuration) => configuration.Enrich.FromLogContext().WriteTo.Console())
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
                 .AddInfrastructure();
-
