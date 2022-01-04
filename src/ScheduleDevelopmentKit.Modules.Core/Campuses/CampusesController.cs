6b47f51e2a85741b4cd650e792dda2d2bf416549
Bibletoon <alex8.ivanov@gmail.com>
2022-01-04 11:54:34 +0300

    Feat: add create campus method

---
+++ src/ScheduleDevelopmentKit.Modules.Core/Campuses/CampusesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ScheduleDevelopmentKit.Modules.Core.Campuses.Commands;
+
+namespace ScheduleDevelopmentKit.Modules.Core.Campuses
+{
+    [ApiController]
+    [Route("api/v1/campuses")]
+    public class CampusesController
+    {
+        private readonly IMediator _mediator;
+
+        public CampusesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<CreateCampus.Response>> CreateTeacher(CreateCampus.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
---
+++ src/ScheduleDevelopmentKit.Modules.Core/Campuses/Commands/CreateCampus.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using JetBrains.Annotations;
+using MediatR;
+using ScheduleDevelopmentKit.DataAccess;
+using ScheduleDevelopmentKit.Domain.Entities;
+using ScheduleDevelopmentKit.Domain.ValueObjects;
+
+namespace ScheduleDevelopmentKit.Modules.Core.Campuses.Commands
+{
+    public class CreateCampus
+    {
+        [PublicAPI]
+        public record Command(
+            string Name,
+            string Address) : IRequest<Response>;
+
+        [UsedImplicitly]
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(c => c.Name).MaximumLength(100);
+                RuleFor(c => c.Address).MaximumLength(300);
+            }
+        }
+
+        [PublicAPI]
+        public record Response(Guid Id, string Name);
+
+        [UsedImplicitly]
+        public class CommandHandler : IRequestHandler<Command, Response>
+        {
+            private readonly SdkDbContext _sdkDbContext;
+
+            public CommandHandler(SdkDbContext context)
+            {
+                _sdkDbContext = context;
+            }
+
+            public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var campus = new Campus(
+                    Guid.NewGuid(),
+                    new CampusName(request.Name),
+                    new CampusAddress(request.Address));
+
+                await _sdkDbContext.Campuses.AddAsync(campus, cancellationToken);
+                await _sdkDbContext.SaveChangesAsync(cancellationToken);
+
+                return new Response(campus.Id, campus.Name.Value);
+            }
+        }
+    }
+}
